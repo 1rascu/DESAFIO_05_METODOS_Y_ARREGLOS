44c14f1d82ddefcba12f82000967489e2a4ceb36
1rascu <mantencion_rc_@hotmail.co>
2024-07-07 01:27:12 -0400

    se modifican todas las hojas para subir el desafio

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,52 +3,37 @@ const total = document.querySelector('#Total')
 const realizados = document.querySelector('#Realizadas')
 const filas = document.querySelector('#Filas')
 const btnAgregar = document.querySelector('#Agregar')
+let idTarea = 4 
+
+
 
 const listadoTareas = [
     {
-        id:1,
-        tarea: 'EJEMPLO 1',
-        estado: false
+        id:1,tarea: 'EJEMPLO 1',estado: false
     },
     {
-        id:12,
-        tarea: 'EJEMPLO 2',
-        estado: true
+        id:2,tarea: 'EJEMPLO 2',estado: false
     },
     {
-        id:12,
-        tarea: 'EJEMPLO 3',
-        estado: false
+        id:3,tarea: 'EJEMPLO 3',estado: false
     }
 ]
-function conteo(){
-    const conteo = listadoTareas.filter(tarea => tarea.estado === true).length
-    realizados.innerHTML = conteo
-}
-function Total() {
-    let contador = listadoTareas.length
-    total.innerHTML = contador
-}
+
 
 RenderListadoTareas = () =>{
     let html = ''
     for (let listado of listadoTareas){
-        html += `<div class="col-3 text-center" style="color: ${listado.estado ? 'green; text-decoration:line-through;'  : 'black'};">
-                <strong>${listado.id}</strong> 
-            </div>
-            <div class="col-3" style="color: ${listado.estado ? 'green ; text-decoration:line-through;' : 'black'};">
-                ${listado.tarea}
-            </div>
+        html += `<div class="col-3 text-center" style="color: ${listado.estado ? 'black; text-decoration:line-through;'  : 'black'};">
+                <strong>${listado.id}</strong> </div>
+            <div class="col-3" style="color: ${listado.estado ? 'black ; text-decoration:line-through;' : 'black'};">
+                ${listado.tarea}</div>
             <div class="col-1">
-                <input type="checkbox" ${listado.estado ? 'checked' : ''} onchange="ActualizarEstado(${listado.id}, this.checked)">
-            </div>
+                <input type="checkbox" ${listado.estado ? 'checked' : ''} onchange="ActualizarEstado(${listado.id}, this.checked)"></div>
             <div class="col-3">
-                <img class="btn_eliminar_tarea" src="./assets/img/btn_cancelar.png" alt="imagen deeliminar" onclick="Borrar(${listado.id})">
-
-            </div>`   
-}filas.innerHTML = html
-Total()
-conteo()
+                <img class="btn_eliminar_tarea" src="./assets/img/btn_cancelar.png" alt="imagen deeliminar" onclick="Borrar(${listado.id})"></div>`   
+        }filas.innerHTML = html
+            Total()
+            conteo()
 }
 
 RenderListadoTareas()
@@ -61,19 +46,27 @@ btnAgregar.addEventListener('click',()=>{
             text: '¡No, se puede guardar una tarea sin datos!',
         });
     } else {
-    tarea = {id:Date.now(),tarea:nuevaTarea.value,estado:false}
+    tarea = {id:idTarea++,tarea:nuevaTarea.value,estado:false}
     listadoTareas.push(tarea)
     nuevaTarea.value = ''
     RenderListadoTareas()
 }})
+
+function conteo(){
+    const conteo = listadoTareas.filter(tarea => tarea.estado === true).length
+    realizados.innerHTML = conteo
+}
+
+function Total() {
+    let contador = listadoTareas.length
+    total.innerHTML = contador
+}
 
 function Borrar(id){
     const index = listadoTareas.findIndex(tarea => tarea.id === id)
     listadoTareas.splice(index,1)
     RenderListadoTareas()
 }
-
-
 
 function ActualizarEstado(id,estado){
     const index = listadoTareas.findIndex(tarea => tarea.id === id)
